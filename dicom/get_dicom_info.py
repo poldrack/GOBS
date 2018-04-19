daafb1096636570f389731959e3681c6702fae06
poldrack <poldrack@gmail.com>
2018-04-19 15:17:12 -0700

    initial add

---
+++ dicom/get_dicom_info.py
@@ -0,0 +1,29 @@
+"""
+load dicom dirs and record what's in them
+"""
+
+import os,glob
+import pydicom
+import pickle
+
+
+basedir='/Users/poldrack/data_unsynced/GOBS/GOBS_dicom'
+
+dcmdirs=glob.glob(os.path.join(basedir,'*'))
+
+dcminfo={}
+for d in dcmdirs:
+    subcode=os.path.basename(d)
+    dcminfo[d]={}
+    scans=glob.glob(os.path.join(d,'SCANS/*'))
+    for s in scans:
+        scannum=int(os.path.basename(s))
+        dcmfiles=glob.glob(os.path.join(s,'DICOM/*_*.dcm'))
+        if len(dcmfiles)==0:
+            dcminfo[d][scannum]=(None,0)
+        else:
+            ds = pydicom.dcmread(dcmfiles[1])
+            dcminfo[d][scannum]=ds.ProtocolName
+
+with open('dcminfo.pkl','wb') as f:
+    pickle.dump(dcminfo,f)
