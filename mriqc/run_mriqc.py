adc31b7bc853f707aad3317b9c9910d1b2022598
Russ Poldrack <poldrack@gmail.com>
2018-04-21 09:54:13 -0500

    custom work dir

--- mriqc/run_mriqc.py
+++ mriqc/run_mriqc.py
@@ -9,6 +9,7 @@ ld --nprocs 32 --mem_gb 64 --ants-nthreads 8 --ants-float --ica
 ids/derivatives participant --participant_label sub-EQ0058 -m T1w --nprocs 16 --
 mem_gb 64 --ants-nthreads 8 --ants-float
 
+need to specify work dir so it doesn't use pwd
 """
 
 import os
@@ -19,19 +20,20 @@ layout = BIDSLayout(project_root)
 
 MRIQC='/work/03843/crn_plab/singularity/poldracklab_mriqc_0.10.4-2018-03-23-f1e4d5941533.img'
 derivdir=os.path.join(project_root,'derivatives')
+workdir='/scratch/01329/poldrack/GOBS/workdir/mriqc'
 
 t1data=layout.get(type='T1w',target='subject',return_type='id')
 
 with open('mriqc_T1w.sh','w') as f:
     for subcode in t1data:
-        cmd='singularity run %s %s %s participant --participant_label sub-%s -m T1w --nprocs 16 --mem_gb 64 --ants-nthreads 8 --ants-float'%(MRIQC,
-                project_root,derivdir,subcode)
+        cmd='singularity run %s %s %s participant --participant_label sub-%s -m T1w --nprocs 16 --mem_gb 64 --ants-nthreads 8 --ants-float -w %s'%(MRIQC,
+                project_root,derivdir,subcode,workdir)
         f.write(cmd+'\n')
 
 bolddata=layout.get(type='bold',target='subject',return_type='id')
 
 with open('mriqc_bold.sh','w') as f:
     for subcode in bolddata:
-        cmd='singularity run %s %s %s participant --participant_label sub-%s -m bold --ica --nprocs 32 --mem_gb 64 --ants-nthreads 8 --ants-float'%(MRIQC,
-                project_root,derivdir,subcode)
+        cmd='singularity run %s %s %s participant --participant_label sub-%s -m bold --ica --nprocs 32 --mem_gb 64 --ants-nthreads 8 --ants-float -w %s'%(MRIQC,
+                project_root,derivdir,subcode,workdir)
         f.write(cmd+'\n')
