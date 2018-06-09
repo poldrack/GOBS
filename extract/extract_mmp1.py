ebdbca20f8bf429fef014bde3b893a02f80f4875
Russell Poldrack <poldrack@gmail.com>
2018-06-09 12:08:27 -0500

    working with cortex

--- extract/extract_mmp1.py
+++ extract/extract_mmp1.py
@@ -1,13 +1,13 @@
 # extract data from MMP1 atlas
 
 
-import os,glob
+import os,glob,sys
 import nibabel
 import numpy
 
 
 basedir_input='/scratch/01329/poldrack/GOBS/GOBS_bids/derivatives/fmriprep'
-subcode='sub-EM2204'
+subcode=sys.argv[1] #'sub-EM2204'
 
 # load atlas surface
 atlasdir='/work/01329/poldrack/stampede2/code/GOBS/extract/HCP-MMP1'
@@ -21,10 +21,14 @@ for hemis in ['L','R']:
     funcfiles[hemis]=glob.glob(os.path.join(basedir_input,subcode,'func/*task-rest_run-1_bold_space-fsaverage5.%s.func.gii'%hemis))[0]
 
 roidata=numpy.zeros((360,150)) # 360 ROIs by 150 timepoints
-
+offset={'L':0,'R':180}
 for hemis in funcfiles:
     print(hemis,funcfiles[hemis])
     funcdata=nibabel.load(funcfiles[hemis])
-    for region in range(1,181):
-        
- 
+    for region in range(180):
+       regionverts=atlasdata[hemis]==region+1
+       for tp in range(150):
+          roidata[region+offset[hemis],tp]=numpy.mean(funcdata.darrays[tp].data[regionverts])
+
+outfile=os.path.join(basedir_input,subcode,'func/HCP-MMP1.roidata.txt')           
+numpy.savetxt(outfile,roidata) 
